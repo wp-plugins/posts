3c3753b12521a0a7074e498c3d38c8f0c7261ac6
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-11 10:21:42 +0000

    Few changes
    git-svn-id: https://plugins.svn.wordpress.org/posts/tags/2.0@1243245 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- next-post.php
+++ next-post.php
@@ -1,8 +1,13 @@
 <?php
 /*
-Plugin Name: Up next Posts
+Plugin Name: Next Posts
+Author: umarbajwa
+Description: Create Next posts boxes in minutes for your WordPress websites.
+Plugin URI: http://web-settler.com/
+Author URI: http://web-settler.com/
+Version: 2.0
+Donate link: http://web-settler.com/
 */
-	
 
 	include 'includes.php';
 
